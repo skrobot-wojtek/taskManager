9aeb1d294df0d84647e6bacc7dcffe2c6dc6af8b
skrobotwojciech <wojtekskrobot1994@gmail.com>
2023-05-27 23:49:36 +0200

    second commit

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -4,8 +4,26 @@ package pl.coderslab;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        options();
 
     }
 
+    static final String file = "tasks.csv";
+    static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+    static String[][] tasks;
+
+
+    // options
+    public static void options() {
+        System.out.println("Welcome in Task Manager");
+        System.out.println(ConsoleColors.BLUE);
+        System.out.println("Choose 1 of the 4 options and type it");
+        System.out.println(ConsoleColors.RESET);
+        System.out.println("add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
+
+    }
 }
+
